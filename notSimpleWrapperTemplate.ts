fb95c369179761b5b5ac7dac7dd97072016a35c6
andzheyevskiy <Igor_lifeking@hotmail.com>
2024-10-01 12:13:11 +0200

    added error handling to REST wrapper

--- notSimpleWrapperTemplate.ts
+++ notSimpleWrapperTemplate.ts
@@ -3,46 +3,94 @@ const restApiv2 = (function () {
     const root = "API root url"
 
     async function _doGet(url: string, needData: boolean, cb?: callback, abortSignal?: AbortController) {
-        const response = await fetch(url, {
-            method: "GET",
-            signal: abortSignal?.signal
-        })
-        const json: object = await response.json() as Object
-        const data = needData ? json : response
-        if (cb) { return cb(data) }
-        return data
+        try {
+            const response = await fetch(url, {
+                method: "GET",
+                signal: abortSignal?.signal
+            })
+
+            if (!response.ok) {
+                throw new Error(`HTTP ERROR: ${response.status}`)
+            }
+            try {
+                const json: object = await response.json() as Object
+                const data = needData ? json : response
+                if (cb) { return cb(data) }
+                return data
+            } catch {
+                throw new Error("Failed to parse response as JSON")
+            }
+        } catch (error) {
+            console.error("Error fetching data: ", error)
+            throw error
+        }
     }
 
     async function _doPost(url: string, body: object, needData: boolean, cb?: callback, abortSignal?: AbortController) {
-        const request = await fetch(url, {
-            method: "POST",
-            body: JSON.stringify(body),
-            signal: abortSignal?.signal
-        })
-        const json: object = await request.json() as Object
-        const data = needData ? json : request
-        if (cb) { return cb(data) }
-        return data
+
+        try {
+            const request = await fetch(url, {
+                method: "POST",
+                body: JSON.stringify(body),
+                signal: abortSignal?.signal
+            })
+
+            if (!request.ok) {
+                throw new Error(`HTTP ERROR: ${request.status}`)
+            }
+            try {
+                const json: object = await request.json() as Object
+                const data = needData ? json : request
+                if (cb) { return cb(data) }
+                return data
+            } catch {
+                throw new Error("Failed to parse resposne as JSON")
+            }
+        } catch (error) {
+            console.error("Error fetching data: ", error)
+            throw error
+        }
     }
 
     async function _doPut(url: string, body: object, needData: boolean, cb?: callback, abortSignal?: AbortController) {
-        const request = await fetch(url, {
-            method: "PUT",
-            body: JSON.stringify(body),
-            signal: abortSignal?.signal
-        })
-        const json: object = await request.json() as Object
-        const data = needData ? json : request
-        if (cb) { return cb(data) }
-        return data
+        try {
+            const request = await fetch(url, {
+                method: "PUT",
+                body: JSON.stringify(body),
+                signal: abortSignal?.signal
+            })
+
+            if (!request.ok) {
+                throw new Error(`HTTP ERROR: ${request.status}`)
+            }
+            try {
+                const json: object = await request.json() as Object
+                const data = needData ? json : request
+                if (cb) { return cb(data) }
+                return data
+            } catch {
+                throw new Error("Failed to parse resposne as JSON")
+            }
+        } catch (error) {
+            console.error("Error fetching data: ", error)
+            throw error
+        }
     }
 
     async function _doDelete(url: string, abortSignal?: AbortController) {
-        const request = await fetch(url, {
-            method: "DELETE",
-            signal: abortSignal?.signal
-        })
-        return request
+        try {
+            const request = await fetch(url, {
+                method: "DELETE",
+                signal: abortSignal?.signal
+            })
+            if (!request.ok) {
+                throw new Error(`HTTP ERROR: ${request.status}`)
+            }
+            return request
+        } catch (error) {
+            console.error("Error fetching data: ", error)
+            throw error
+        }
     }
 
     function _singularGet(path: string, needData: boolean) {
@@ -71,7 +119,7 @@ const restApiv2 = (function () {
         }
     }
 
-    function _updateItem(path: string, needData:boolean) {
+    function _updateItem(path: string, needData: boolean) {
         return function (id: number, body: object, cb?: callback, abortSignal?: AbortController) {
             return _doPut(root + path + "/" + id, body, needData, cb, abortSignal)
         }
@@ -90,14 +138,14 @@ const restApiv2 = (function () {
         // This methods return the json file of the request
         getPerson: _singularGet("people", true),
         getPeople: _pluralGet("people", true),
-        createPerson: _createItem("people",true),
-        updatePerson: _updateItem("people",true),
+        createPerson: _createItem("people", true),
+        updatePerson: _updateItem("people", true),
 
         // This methods return the promise of the request
         requestPerson: _singularGet("people", false),
         requestPeople: _pluralGet("people", false),
-        requestCreatePerson: _createItem("people",false),
-        requestUpdatePerson: _updateItem("people",false),
+        requestCreatePerson: _createItem("people", false),
+        requestUpdatePerson: _updateItem("people", false),
         deletePerson: _deleteItem("people")
     }
 })()
