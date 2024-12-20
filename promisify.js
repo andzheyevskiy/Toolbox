01f5536038cd166c06f950ce4ee6a87afc253c85
Igor <41655195+andzheyevskiy@users.noreply.github.com>
2024-12-20 11:24:35 +0100

    Added error handling and multiple result control to promisify

--- promisify.js
+++ promisify.js
@@ -1,11 +1,14 @@
 function promisify(fn) {
+  if(typeof fn !== "function"){
+    return TypeError("Provided argument is not a function")
+  }
   return function (...args) {
     return new Promise((resolve, reject) => {
-      fn(...args, (err, data) => {
+      fn(...args, (err, ...data) => {
         if (err) {
           reject(err)
         } else {
-          resolve(data)
+          resolve(data.length === 1? data[0]: data)
         }
       })
     })
