942525c38b33c83e33ca05f0056bc52ec577b673
andzheyevskiy <Igor_lifeking@hotmail.com>
2025-05-14 13:12:56 +0200

    (semicolon asertion for consistency)

--- traductorSimpleWordpress.js
+++ traductorSimpleWordpress.js
@@ -11,18 +11,18 @@ class TraductorWP {
             throw new Error("El argumento 'elementos' tiene que ser un Array de strings");
         } else {
             this.elementos.forEach(e => {
-                if(typeof e !== "string") throw new Error("El argumento 'elementos' tiene que ser un Array de strings")
-                this.dom.push(...document.querySelectorAll(e))
+                if(typeof e !== "string") throw new Error("El argumento 'elementos' tiene que ser un Array de strings");
+                this.dom.push(...document.querySelectorAll(e));
             })
         }
     }
 
     traducir(original, nuevo) {
         if (typeof original != "string" || typeof nuevo != "string") {
-            throw new Error("Argumentos 'original' y 'nuevo' tienen que ser string")
+            throw new Error("Argumentos 'original' y 'nuevo' tienen que ser string");
         } else {
             this.dom.forEach(e=>{
-                if(e.textContent.trim().toLowerCase() === original.trim().toLowerCase()) e.textContent = nuevo
+                if(e.textContent.trim().toLowerCase() === original.trim().toLowerCase()) e.textContent = nuevo;
             })
         }
     }
@@ -32,11 +32,11 @@ class TraductorWP {
 //======== USO ========//
 // Añadir este js a <head> con defer
 
- const elementosATraducir = ["h1","h2","h3", "h4", "h5", "p", "span"] // meter o quitar elementos HTML objetivo, admite cualquier selector CSS
- const traductor = new TraductorWP(elementosATraducir) // NO TOCAR
+ const elementosATraducir = ["h1","h2","h3", "h4", "h5", "p", "span"]; // meter o quitar elementos HTML objetivo, admite cualquier selector CSS
+ const traductor = new TraductorWP(elementosATraducir); // NO TOCAR
 
 
 // Aqui meteis los textos a traducir.
 // traductor.traducir(texto original, texto traducido)
 // texto original es insensitivo
-traductor.traducir("hello world", "Hola Mundo")
+traductor.traducir("hello world", "Hola Mundo");
